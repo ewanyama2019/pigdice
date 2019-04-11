12a7ae28b61743579ca37ee4ac370fd1fe602408
Eric Wanyama <wafulawanyama@gmail.com>
2019-04-11 15:31:23 +0300

    added Random javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
-    console.log("Page is Ready");
+    // console.log("Page is Ready");
+    var scores_1= [];
+    var cumulative_score_1 =0;
+    
+    $("button#rollDice").click(function () {
+
+        var min = 1;
+        var max = 7;
+        var random = Math.floor(Math.random() * (+max - +min)) + +min;
+        
+        $("h2#player1_roll").prepend(random +", ");
+        scores_1.push(random)
+
+        var sum = _.reduce([1, 2, 3], function (memo, num) { return memo + num; }, 0);
+
+    });
+
 });
 
-function player(name, score) {
-    this.nameOfPlayer = name;
-    this.cummScore = score;
-    this.diceRoll = [];
-};
 
-var player1 = new player (Player1)
+
+
+
+
+// /*  Constructor for Player*/
+// function player(name, score) {
+//     this.nameOfPlayer = name;
+//     this.cummScore = score;
+//     this.diceRoll = [];
+// };
+
+// /*  Declared object player*/ 
+// var player1 = new player ("Player1" );
+
