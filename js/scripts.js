f63c7fa927551e1c50f18cd58e010929fa4bd9ea
Eric Wanyama <wafulawanyama@gmail.com>
2019-04-11 19:08:13 +0300

    added 2 buttons for player 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // console.log("Page is Ready");
-    var scores_1= [];
-    var cumulative_score_1 =0;
+    var scores_1 = [];
+    var cumulative_score_1 = 0;
     var player1Score;
 
     $("button#rollDice").click(function () {
@@ -9,22 +9,24 @@ $(document).ready(function(){
         var min = 1;
         var max = 7;
         var random = Math.floor(Math.random() * (+max - +min)) + +min;
-        
-        $("h2#player1_roll").prepend(random +", ");
+
+        $("h2#player1_roll").prepend(random + ", ");
         scores_1.push(random);
 
-        var sum1 = _.reduce(scores_1, function (memo, num) { return memo + num; }, 0);
+        var sum1 = _.reduce(scores_1, function (memo, num) {
+            return memo + num;
+        }, 0);
         // alert(sum1);
 
         $("button#hold").click(function () {
             cumulative_score_1 = sum1;
-            
+
             // alert(cumulative_score_1);
 
             $("h2#player1_cum_score").text(cumulative_score_1);
 
         });
-        
+
 
     });
 
@@ -44,4 +46,3 @@ $(document).ready(function(){
 
 // /*  Declared object player*/ 
 // var player1 = new player ("Player1" );
-
