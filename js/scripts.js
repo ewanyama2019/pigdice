5836561a8f7d501345657ddcbebc95d57e93a73b
Eric Wanyama <wafulawanyama@gmail.com>
2019-04-11 18:20:58 +0300

    added logic to display cummulative score in the scores div

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     // console.log("Page is Ready");
     var scores_1= [];
     var cumulative_score_1 =0;
+    var player1Score;
 
     $("button#rollDice").click(function () {
 
@@ -13,7 +14,17 @@ $(document).ready(function(){
         scores_1.push(random);
 
         var sum1 = _.reduce(scores_1, function (memo, num) { return memo + num; }, 0);
-        alert(sum1);
+        // alert(sum1);
+
+        $("button#hold").click(function () {
+            cumulative_score_1 = sum1;
+            
+            // alert(cumulative_score_1);
+
+            $("h2#player1_cum_score").text(cumulative_score_1);
+
+        });
+        
 
     });
 
