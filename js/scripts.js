8124277c63711c69897d2257c087c2c091488db8
Eric Wanyama <wafulawanyama@gmail.com>
2019-04-14 03:19:04 +0300

    added init function with clas reset event handlers

--- js/scripts.js
+++ js/scripts.js
@@ -51,7 +51,9 @@ $(document).ready(function () {
     $("button#initialize").click(function () {
         location.reload();
     });
-//Update player Visuals
+
+function init() {    
+    //Update player Visuals
     document.getElementById('player1').innerText = 'Player1';
     document.getElementById('player2').innerText = 'Player2';
     document.getElementById('player1_cum_score').innerText = '0';
@@ -60,64 +62,77 @@ $(document).ready(function () {
     document.getElementById('player2_roll').innerText = '0';
     document.querySelector('.dice').style.display = 'none';
 
-    $("button#rollDice1").click(function () {
-        player1.Roll();
+//classes reset 
+    document.querySelector('.player1-banner').classList.remove('active');
+    document.querySelector('.player1-banner').classList.remove('winner');
+    ducument.querySelector('.player2-banner').classList.remove('active');
+    document.querySelector('.player2-banner').classList.remove('winner');
+    document.querySelector('.player1-banner').classList.add('active');
 
-        if (_.contains(player1.diceRoll, 1)) {
-            alert("You're out");
-            player1.diceRoll = [];
-            // $("h2#player1_roll").text(player1.diceRoll);
-        } else if ($("button#hold1").click(function () {}) && (_.contains(player1.diceRoll, 1))) {} else {
-
-            $("h2#player1_roll").text(player1.diceRoll + ", ");
-            $("button#hold1").click(function () {
-                player1.Hold();
-                $("h2#player1_cum_score").text(player1.roundScore);
-                if (player1.cummScore === 100) {
-                    alert("Player 1 Wins")
-                }
-
-            });
-        };
+};
+    init();
+//Dice roll
+document.querySelector('#rollDice1').addEventListener('click', function() {
+    
+    player1.Roll();
+});
 
 
+//     $("button#rollDice1").click(function () {
+//         player1.Roll();
 
+//         if (_.contains(player1.diceRoll, 1)) {
+//             alert("You're out");
+//             player1.diceRoll = [];
+//             // $("h2#player1_roll").text(player1.diceRoll);
+//         } else if ($("button#hold1").click(function () {}) && (_.contains(player1.diceRoll, 1))) {} else {
 
+//             $("h2#player1_roll").text(player1.diceRoll + ", ");
+//             $("button#hold1").click(function () {
+//                 player1.Hold();
+//                 $("h2#player1_cum_score").text(player1.roundScore);
+//                 if (player1.cummScore === 100) {
+//                     alert("Player 1 Wins")
+//                 }
 
-    });
-
-
-    $("button#rollDice2").click(function () {
-        player2.Roll();
-
-        if (_.contains(player2.diceRoll, 1)) {
-            alert("You're out");
-            player2.diceRoll = [];
-            // $("h2#player2_roll").text(player2.diceRoll);
-        } else if ($("button#hold2").click(function () {}) && (_.contains(player2.diceRoll, 1))) {} else {
-
-            $("h2#player2_roll").text(player2.diceRoll + ", ");
-            $("button#hold2").click(function () {
-                player2.Hold();
-                $("h2#player2_cum_score").text(player2.roundScore);
-                if (player2.cummScore === 100) {
-                    alert("Player 2 Wins")
-                }
-
-            });
-        };
+//             });
+//         };
 
 
 
 
 
-    });
+//     });
+
+
+//     $("button#rollDice2").click(function () {
+//         player2.Roll();
+
+//         if (_.contains(player2.diceRoll, 1)) {
+//             alert("You're out");
+//             player2.diceRoll = [];
+//             // $("h2#player2_roll").text(player2.diceRoll);
+//         } else if ($("button#hold2").click(function () {}) && (_.contains(player2.diceRoll, 1))) {} else {
+
+//             $("h2#player2_roll").text(player2.diceRoll + ", ");
+//             $("button#hold2").click(function () {
+//                 player2.Hold();
+//                 $("h2#player2_cum_score").text(player2.roundScore);
+//                 if (player2.cummScore === 100) {
+//                     alert("Player 2 Wins")
+//                 }
+
+//             });
+//         };
+
+
+
+
+
+//     });
 
 
 
 
 
 });
-
-document.getElementById('player1').innerText = 'Player1';
-document.getElementById('player2').innerText = 'Player2';
